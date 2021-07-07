8df2efbf70246358a6cb9bd6aff4743be610ac0f
Brittany Garrett <brittanymgarrett@gmail.com>
2021-07-06 22:41:32 -0500

    Rounds.js updated

--- src/scripts/rounds.js
+++ src/scripts/rounds.js
@@ -1,98 +1,71 @@
 /* eslint-disable indent */
-import { getTeams, fetchTeams } from './dataAccess.js';
+import { getTeams, fetchTeams, saveGame } from './dataAccess.js';
 
 const roundsBody = document.querySelector('#rounds-container'); 
 
 fetchTeams().then(() => {
-    roundsBody.innerHTML = `${roundOne()} ${roundTwo()} ${roundThree()}`;
+    roundsBody.innerHTML = `${roundPage()}`;
   });
 
-//roundOne will collect total scores and display in table
-//   scores [{   
-//     "id" : 1,
-//     "roundId" : 1,
-//     "teamId" :  1,
-//     "score" : 5
-// }]
+  
+const roundPage = () => {
+    let html = '';
+    html += ` 
+    ${roundTable()}
+    ${roundTable()}
+    ${roundTable()}
+    <button id="save_game">Save Game</button><br>
+    `;
+    return html; 
+};
 
-const roundOne = () => {
+
+  const roundTable = () => {
+      
+      const teams = getTeams();
+
+      return `
+      <div class="round_table">
+      <table>
+      <thead>
+      <tr>
+      <th>Round Score</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+      ${teams.map(team => {
+          return `
+          <td value="${team.id}">${team.teamName}</td>
+          <td><input class="points" type="number" id="${team.id}"/></td>
+          `;
+        }).join('')}
+        </tr>
+        </tbody>
+        </table>
+        </div>
+        `;
+    };
     
-    const teams = getTeams();
+    roundsBody.addEventListener('click', e => {
+        if (e.target.id === 'save_game') {
+            const teamOneGame = parseInt(document.querySelectorAll('.points')[0].value) + parseInt(document.querySelectorAll('.points')[3].value) + parseInt(document.querySelectorAll('.points')[6].value);
+            const teamTwoGame = parseInt(document.querySelectorAll('.points')[1].value) + parseInt(document.querySelectorAll('.points')[4].value) + parseInt(document.querySelectorAll('.points')[7].value);
+            const teamThreeGame = parseInt(document.querySelectorAll('.points')[2].value) + parseInt(document.querySelectorAll('.points')[5].value) + parseInt(document.querySelectorAll('.points')[8].value);
 
-  return `
-      <div class="roundTable">
-        <table>
-            <thead>
-                <tr>
-                    <th>Round One Score</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                ${teams.map(team => {
-                return `
-                    <td value="${team.id}">${team.teamName}</td>
-                    <td><input type="text"/></td>
-                    `;
-                      }).join('')}
-                </tr>
-            </tbody>
-        </table>
-      </div>
-      `;
-};
 
-const roundTwo = () => {
-
-    const teams = getTeams();
-
-return `
-        <div class="roundTable">
-        <table>
-            <thead>
-                <tr>
-                    <th>Round Two Score</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                ${teams.map(team => {
-                return `
-                    <td value="${team.id}">${team.teamName}</td>
-                    <td><input type="text"/></td>
-                    `;
-                      }).join('')}
-                </tr>
-            </tbody>
-        </table>
-      </div>
-      `;
-};
-
-const roundThree = () => {
-
-    const teams = getTeams();
-
-return `
-        <div class="roundTable">
-        <table>
-            <thead>
-                <tr>
-                    <th>Round Three Score</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                ${teams.map(team => {
-                return `
-                    <td value="${team.id}">${team.teamName}</td>
-                    <td><input type="text"/></td>
-                    `;
-                      }).join('')}
-                </tr>
-            </tbody>
-        </table>
-      </div>
-      `;
-};
-
+            const sendToAPI = {
+                teamOne : teamOneGame,
+                teamTwo : teamTwoGame,
+                teamThree : teamThreeGame
+            };
+        
+            // if(document.querySelectorAll('.points').every(input => {
+            //      return input.value > 0;
+            // })){
+                saveGame(sendToAPI);
+        //     } else {
+        //         alert('Please enter all scores before submitting team points!');
+        //     }
+        // }
+    }});
